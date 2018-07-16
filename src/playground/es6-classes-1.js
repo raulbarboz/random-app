37cec253d91becd96a1f464491a8743554b6d19a
Raul Barboza <ruzito@gmail.com>
2018-07-15 22:53:24 -0300

    classes 1 done!

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -1,15 +1,21 @@
 class Person {
-  constructor(name = 'Anonymous') {
+  constructor(name = 'Anonymous', age = 0) {
     this.name = name;
+    this.age = age;
   }
   getGretting() {
     //return this.name + ' says Hi!';
     return `Hi. I am ${this.name}`
   }
+  getDescription() {
+    //return this.name + ' says Hi!';
+    return `${this.name} is ${this.age} years old`
+  }
 }
 
-const me = new Person('Raul Barboza');
+const me = new Person('Raul Barboza', 31);
 console.log(me.getGretting());
+console.log(me.getDescription());
 
 const other = new Person();
 console.log(other.getGretting());
